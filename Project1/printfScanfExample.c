c85817ba3e2b2c34c2c537bed06bc93a489469fa
younhoda <qwe132567@naver.com>
2023-01-23 21:11:54 +0900

    주석처리내용

---
+++ Project1/printfScanfExample.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void) {
+	
+	/*int age = 12;
+	printf("%d\n", age);
+	age = 13;
+	printf("%d\n", age);*/
+	
+
+	printf("1\n");
+	/*printf("2\n");*/ //2는 범위에서 벗어나므로 주석처리
+	printf("3\n");
+
+
+	return 0;
+}
