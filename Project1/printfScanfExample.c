a641ae7bc68e848ccaac2bee871040ac8f911803
younhoda <qwe132567@naver.com>
2023-01-23 21:16:06 +0900

    실수형변수에 대한 예제

--- Project1/printfScanfExample.c
+++ Project1/printfScanfExample.c
@@ -8,9 +8,15 @@ int main(void) {
 	printf("%d\n", age);*/
 	
 
-	printf("1\n");
-	/*printf("2\n");*/ //2는 범위에서 벗어나므로 주석처리
-	printf("3\n");
+	//printf("1\n");
+	///*printf("2\n");*/ //2는 범위에서 벗어나므로 주석처리
+	//printf("3\n");
+
+	//실수형 변수에 대한 예제
+	float weight = 55.5;
+	printf("%.2f\n", weight);
+	double d=3.14592;
+	printf("%.2lf\n", d);
 
 
 	return 0;
