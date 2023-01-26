61cd249dff04284b395fc72e21561650498abfd9
younhoda <qwe132567@naver.com>
2023-01-26 23:44:26 +0900

    반복문 예제 샘플

---
+++ Project1/ForWhileDoWhileExample.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main(void) {
+
+	//int a = 10;
+	//printf("a는 %d\n", a);
+	//a++; //a=10+1
+	//printf("a는 %d\n", a);
+	//a++; //a=11+1
+	//printf("a는 %d\n", a);
+	//
+	//int b = 20;
+	//printf("a는 %d\n", ++b); //b = b +1
+	//printf("a는 %d\n", b++); //b = b +1
+	//printf("a는 %d\n", b); // b +1
+
+
+	/*int i = 1;
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);
+	printf("Hello world %d\n", i++);*/
+
+
+	//선언 조건 증감
+	/*for (int i = 1; i <= 10; i++) {
+		printf("Hello world %d\n", i);
+	}*/
+
+	//while 조건() {} 
+	/*int i = 1;
+	while (i<=10) {
+		printf("Hello world %d\n", i);
+		i++;
+	}*/
+	
+	//do {} while(조건)
+	/*int i = 1;
+	do {
+		printf("Hello world %d\n", i);
+	} while (i <= 10);*/
+
+	/*for (int i = 2; i <=9; i++) {
+		for (int j = 1; j<=9; j++) {
+			printf("i * j=%d%d\n",i+j,i*j);
+		}
+		printf("\n");
+	}*/
+	
+	/*for (int i = 1; i <= 3; i++) {
+		printf("첫번째 반복문:%d\n", i);
+	}
+		for (int j = 1; j <= 5; j++) {
+			printf("  두번째 반복문:%d\n", j);
+		}
+		for (int k = 1; k <= 7; k++) {
+			printf("		세번째 반복문:%d\n", k);
+		}*/
+
+
+
+
+
+	return 0;
+}
